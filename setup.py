67de0d45d2933b6869bc1ea78c131e62e0c41a05
Karile Myrr <kirillant2013@gmail.com>
2021-12-29 13:27:47 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='testModuleByKarielMyrr',
+    version='0.1.0',    
+    description='Test module for Data Analysis.',
+    author='Karile_Myrr',
+    author_email='kirillant2013@gmail.com',
+    url='https://github.com/Alex-like/modul-alex1.git',
+    packages=['aiShelModule'],
+    install_requires=['numpy>=1.9.0'],
+
+    classifiers=[ 
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
